5b44e4fcc5ef0d3b4e53071ce0b684f26fd5ab8e
krrprr <krista.partel@gmail.com>
2017-05-17 16:53:05 +0300

    add page header component

---
+++ app/components/page-header.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  headerScroll () {
+    var pageHeader = document.getElementById("page-header");
+    console.log(pageHeader);
+  }
+
+
+});
