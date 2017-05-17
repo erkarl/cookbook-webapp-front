d3118c10710a49f072f8533fcee8cb719ba0a800
krrprr <krista.partel@gmail.com>
2017-05-17 15:30:44 +0300

    remove search attempt

--- app/components/search-recipes.js
+++ app/components/search-recipes.js
@@ -1,20 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['search-recipes'],
-  value: '',
 
-  init() {
-    this._super(...arguments);
-    this.get('search')('').then((results) => this.set('results', results));
-  },
-
-  actions: {
-    handleSearchEntry() {
-      let searchInputValue = this.get('value');
-      let searchAction = this.get('search');
-      searchAction(searchInputValue).then((searchResults) => this.set('results', searchResults));
-    }
-  }
 
 });
