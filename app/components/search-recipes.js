952f235109f624813ffee5470e336adc606261a5
krrprr <krista.partel@gmail.com>
2017-05-20 11:23:13 +0300

    add search feature

--- app/components/search-recipes.js
+++ app/components/search-recipes.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
-
+  actions: {
+    updateSearchQuery(searchQuery) {
+      this.get('searchChanged')(searchQuery);
+    }
+  }
 });
