3854e2fe06aa664655737e14f788b3cf3c14ad91
Karl Ranna <karl@karlranna.com>
2017-05-22 22:10:16 +0300

    Add search recipes tests

--- tests/integration/components/search-recipes-test.js
+++ tests/integration/components/search-recipes-test.js
@@ -5,6 +5,23 @@ moduleForComponent('search-recipes', 'Integration | Component | search recipes',
   integration: true
 });
 
+test('it triggers updateSearchQuery action with pancakes parameter', function(assert) {
 
-test('it triggers updateSearchQuery action on typing', function(assert) {
+  const expectedSearchQuery = 'pancakes';
+  const onUpdateSearchQuery = (actualSearchQuery) => {
+    console.log('onUpdateSearchQuery called with params', actualSearchQuery);
+    assert.equal(actualSearchQuery, expectedSearchQuery, 'Search query equals to pancakes');
+  };
+
+  this.on('updateSearchQuery', onUpdateSearchQuery);
+
+  const onFilterBySearch = (searchQuery) => {
+    console.log('onFilterBySearch called with params', searchQuery);
+  };
+  this.set('filterBySearch', onFilterBySearch);
+
+
+  this.render(hbs`{{search-recipes searchChanged=onFilterBySearch}}`);
+
+  assert.equal(true, false);
 });
